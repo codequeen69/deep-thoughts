6d241aa37c967e2f11380040bde6be4475365402
Venus DM <crhowe90@gmail.com>
2022-10-11 17:47:06 -0500

    server and client working together

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const { authMiddleware } = require('./utils/auth');
 
 //import Apollo server
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
 //import typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -29,6 +30,15 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
   //integrate our Apollo server with the express application as middleware
   server.applyMiddleware({app});
+
+  // Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
   db.once('open', () => {
     app.listen(PORT, () => {
